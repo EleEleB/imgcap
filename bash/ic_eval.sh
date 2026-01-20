68472fa8194c994379ee80ba3be4526ce7dff844
paolo-gajo <paolo.gajo2@unibo.it>
2026-01-20 13:16:34 +0100

    update bash scripts

--- bash/ic_eval.sh
+++ bash/ic_eval.sh
@@ -4,8 +4,8 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=20G
 #SBATCH --time=4:00:00
-#SBATCH --output=ic_eval_%j.out
-#SBATCH --error=ic_eval_%j.err
+#SBATCH --output=./.slurm/%A/%a_output.log
+#SBATCH --error=./.slurm/%A/%a_error.log
 
 start=$(date +%s)
 
--- bash/ic_train.sh
+++ bash/ic_train.sh
@@ -4,8 +4,8 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=20G
 #SBATCH --time=24:00:00
-#SBATCH --output=ic_%j.out
-#SBATCH --error=ic_%j.err
+#SBATCH --output=./.slurm/%A/%a_output.log
+#SBATCH --error=./.slurm/%A/%a_error.log
 
 start=$(date +%s)
 
